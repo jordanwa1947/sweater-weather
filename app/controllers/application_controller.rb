34ca7b123840d13ed8252d8e1c97b8e687ac482e
Jordan Whitten <jordanwa1947@gmail.com>
2019-01-06 20:22:17 -0700

    adds error messages for api with potential for translation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+
+  def render_json_error(status, error_code, extras = {})
+    status = Rack::Utils::SYMBOL_TO_STATUS_CODE[status] if status.is_a? Symbol
+
+    error = {
+      title: I18n.translate("error_messages.#{error_code}.title"),
+      status: status
+    }.merge(extras)
+
+    detail = I18n.translate("error_messages.#{error_code}.detail", default: '')
+    error[:detail] = detail unless detail.empty?
+
+    render json: { errors: [error] }, status: status
+  end
 end

37411e6aa0b5ffd5e4d415be604b0e6689ef6705
Jordan Whitten <jordanwa1947@gmail.com>
2019-01-06 20:32:36 -0700

    sets up favorite model with migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190103230031) do
+ActiveRecord::Schema.define(version: 20190106042312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.string "location"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "password_digest"
@@ -23,4 +29,5 @@ ActiveRecord::Schema.define(version: 20190103230031) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "favorites", "users"
 end

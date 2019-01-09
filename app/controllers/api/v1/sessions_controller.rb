458adcdb4ae409102fb88c024f43f71da979a6cc
Jordan Whitten <jordanwa1947@gmail.com>
2019-01-08 23:53:22 -0700

    left pry in controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,8 +2,6 @@ class Api::V1::SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
-    binding.pry
-
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       render json: UserSerializer.new(user).serializable_hash
